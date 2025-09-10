ee0e4871cf16d24cbbcab45f60acc0a75375736f
zincronito <zincronito@gmail.com>
2025-09-09 22:13:28 -0600

    segundo avance

--- src/com/lexer/Lexer.java
+++ src/com/lexer/Lexer.java
@@ -13,6 +13,114 @@ public class Lexer {
         this.codigo = codigo;
     }
 
+
+    public List<Token> scanTokens() {
+        while (!estaAlFinal()) {
+            inicio = actual;
+            scanToken();
+        }
+        // Anadir el token de fin de archivo
+        tokens.add(new Token(tipoToken.EOF, "", linea, inicio));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = avanzar();
+        switch (c) {
+            // símbolos de un solo carácter
+            case '(': 
+                addToken(tipoToken.PARENTESIS_IZQ);
+                break;
+
+            case ')': 
+                addToken(tipoToken.PARENTESIS_DER);
+                break;
+
+            case '{': 
+                addToken(tipoToken.LLAVE_IZQ);
+                break;
+
+            case '}': 
+                addToken(tipoToken.LLAVE_DER);
+                break;
+
+            case ',': 
+                addToken(tipoToken.COMA);
+                break;
+
+            case ';': 
+                addToken(tipoToken.PUNTO_Y_COMA);
+                break;
+
+            case '+': 
+                addToken(tipoToken.SUMA);
+                break;
+
+            case '-': 
+                addToken(tipoToken.RESTA);
+                break;
+
+            case '*': 
+                addToken(tipoToken.MULTIPLICACION);
+                break;
+
+            // operadores que podrían ser de uno o dos caracteres
+            case '!': 
+                addToken(match('=') ? tipoToken.DIFERENTE : tipoToken.DESCONOCIDO); // '!' solo no es válido
+                break;
+
+            case '=': 
+                addToken(match('=') ? tipoToken.IGUAL : tipoToken.ASIGNACION);
+                break;
+
+            case '<': 
+                addToken(match('=') ? tipoToken.MENOR_IGUAL : tipoToken.MENOR_QUE);
+                break;
+
+            case '>': 
+                addToken(match('=') ? tipoToken.MAYOR_IGUAL : tipoToken.MAYOR_QUE);
+                break;
+
+            // la división es especial porque puede iniciar un comentario
+            case '/':
+                if (match('/')) {
+                    // es un comentario, entonces avanza hasta el final de la línea
+                    while (mirar() != '\n' && !estaAlFinal()) avanzar();
+                } else {
+                    addToken(tipoToken.DIVISION);
+                }
+                break;
+
+            // ignorar espacios en blanco
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+
+            // nueva línea
+            case '\n':
+                linea++;
+                break;
+
+            // literales de cadena
+            case '"':
+                literal();
+                break;
+
+            default:
+                if (esDigito(c)) {
+                    numero();
+                } else if (esLetra(c)) {
+                    identificador();
+                } else {
+                    // carácter no reconocido
+                    addToken(tipoToken.DESCONOCIDO);
+                }
+                break;
+        }
+    }
+
+
     public void identificador() {
         while (esAlfaNumerico(mirar()))
             avanzar();
@@ -86,5 +194,14 @@ public class Lexer {
         // Si el codigo actual no es nulo, se usa ese para cadenas
         String lexema = (literal == null)  ? texto : literal.toString();
         tokens.add(new Token(tipo, lexema, linea, inicio));
+    }
+
+
+    private boolean match(char expected) {
+        if (estaAlFinal() || codigo.charAt(actual) != expected) {
+            return false;
+        }
+        actual++;
+        return true;
     }
 }
--- src/com/lexer/tipoToken.java
+++ src/com/lexer/tipoToken.java
@@ -4,6 +4,8 @@ public enum tipoToken {
     // Palabras reservadas
     // Aqui pongan las palabras reservadas
     
-    // Identificadores y tipos de datos
+    // Identificadores y tipos de datos 
     INT, FLOAT, CADENA
+
+    //yobaniwis agrega EOF "end of file" y todos los que dan error en lexer.java y estan en MAYUSCULAS 
 }
