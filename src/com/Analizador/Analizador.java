5991af3c7c83213ed80cd0a4a9516d744592f288
joel12-Sant <joelsantiagos000@gmail.com>
2025-09-14 19:38:02 -0600

    com

--- src/com/Analizador/Analizador.java
+++ src/com/Analizador/Analizador.java
@@ -4,7 +4,8 @@ import com.gui.CompiladorMain;
 public class Analizador {
 
     public static void main(String[] args) {
-        // Inicia directamente tu clase principal de la interfaz
+        /* Ya integre lo que estaba aqui con la interfaz ahora inicia 
+        directamente la clase principal de la interfaz¨*/
         CompiladorMain.main(args);
     }
 }
